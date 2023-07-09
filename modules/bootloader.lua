5b85e654290f270c1c0c01b4c9d5b73a6768f466
gLiTcH#6948 <nexonalive@gmail.com>
2023-07-09 12:46:43 +0300

    Update bootloader.lua

--- modules/bootloader.lua
+++ modules/bootloader.lua
@@ -1,5 +1,5 @@
 function boot()
-    print("/dev/"..fs.getDir(rom)..": clean, "..fs.getSize(/).."/"..fs.getCapacity.." files, 4146215/121076480 blocks")
+    print("/dev/"..fs.getDir(rom)..": clean, "..fs.getSize("/").."/"..fs.getCapacity("/").." files, 4146215/121076480 blocks")
     print("Starting MeteorOS...")
     os.sleep(3)
     shell.run("/MeteorOS/modules/system.lua")
