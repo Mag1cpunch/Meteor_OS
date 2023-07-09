b5fbee890510b0e037fa7ac021fba78811e8d1f0
gLiTcH#6948 <nexonalive@gmail.com>
2023-07-09 13:56:16 +0300

    Update bootloader.lua

--- modules/bootloader.lua
+++ modules/bootloader.lua
@@ -1,5 +1,9 @@
 function boot()
-    print("/dev/"..fs.getDir("/rom")..": clean, "..fs.getSize("/").."/"..fs.getCapacity("/").." files, 4146215/121076480 blocks")
+    local freespace = getFreeSpace("/")
+    local romsize = fs.getSize("/rom")
+    local meteorsize = fs.getSize("/MeteorOS")
+    local totalspace = meteorsize + romsize + freespace
+    print("/dev/sda1: clean, "..totalspace.."/"..totalspace.." files, 4146215/121076480 blocks")
     print("Starting MeteorOS...")
     os.sleep(3)
     shell.run("/MeteorOS/modules/system.lua")
