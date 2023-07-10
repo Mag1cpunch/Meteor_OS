5667e902a78bf22efd84e9ebc602b5b2ababa59f
gLiTcH#6948 <nexonalive@gmail.com>
2023-07-10 23:41:26 +0300

    Update kernel.lua

--- kernel/kernel.lua
+++ kernel/kernel.lua
@@ -16,12 +16,38 @@ local fstree = {
 function initfs()
     print("Verifying EXT3 FileSystem...")
     os.sleep(3)
-    for _, dirs in pairs(fstree) do
-        if not fs.exists(dirs) then
-            print("EXT3 FileSystem tree not found generating new...")
-            os.sleep(3)
-            fs.makeDir(dirs)
-        end
+    if not fs.exists("/MeteorOS") then
+        fs.makeDir("/MeteorOS")
+    end
+    if not fs.exists("/MeteorOS/modules") then
+        fs.makeDir("/MeteorOS/modules")
+    end
+    if not fs.exists("/MeteorOS/boot") then
+        fs.makeDir("/MeteorOS/boot")
+    end
+    if not fs.exists("/MeteorOS/etc") then
+        fs.makeDir("/MeteorOS/etc")
+    end
+    if not fs.exists("/MeteorOS/usr") then
+        fs.makeDir("/MeteorOS/usr")
+    end
+    if not fs.exists("/MeteorOS/sbin") then
+        fs.makeDir("/MeteorOS/sbin")
+    end
+    if not fs.exists("/MeteorOS/var") then
+        fs.makeDir("/MeteorOS/var")
+    end
+    if not fs.exists("/MeteorOS/tmp") then
+        fs.makeDir("/MeteorOS/tmp")
+    end
+    if not fs.exists("/MeteorOS/dev") then
+        fs.makeDir("/MeteorOS/dev")
+    end
+    if not fs.exists("/MeteorOS/dev/block") then
+        fs.makeDir("/MeteorOS/dev/block")
+    end
+    if not fs.exists("/MeteorOS/dev/sda1") then
+        fs.makeDir("/MeteorOS/dev/sda1")
     end
     print("[ OK ] EXT3 FileSystem Initialized!")
 end
